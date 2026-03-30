e3421178bb1a192419846a86d2c8716ee99b4de5
Cheesydibs <9039@leerling.keizerkarelcollege.nl>
2026-03-30 14:52:07 +0200

    test buttons again

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 function changebackgroundred() {
-            document.body.style.backgroundColor = "red";
+            document.body.style.backgroundColor = "#8f0606";
+            document.body.style.color = "#5bd5f4";
         }    
 
 function changebackgroundblue() {
-            document.body.style.backgroundColor = "blue";
+            document.body.style.backgroundColor = "#5bd5f4";
+            document.body.style.color = "#8f0606";
         }        
 
 
