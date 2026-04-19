3a3df42a47ac2d3dba9c19b586b9de1152280f3c
Cheesydibs <9039@leerling.keizerkarelcollege.nl>
2026-04-19 13:08:55 +0200

    deel 1 security

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
 import (
-  "ftm"
-  "net/http"
-  "time"
+  "ftm"  //is nodig voor bepaalde functies//
+  "net/http"   //zorgt ervoor dat dingen zoals cookies en redirects goed worden gebruikt, denk ik//
+  "time"  //is voor tijd, duh//
 )
+
+type login struct{
+  HashedPassword string
+  SessionToken string
+  CSRFToken string
+}
+
+//usernames worden samen met gegevens in een map bewaard//
+var users = map[string]login{}
+
+//alle functies//
+func main() {
+  http.HandleFunc("/register", register)
+  http.HandleFunc("/login", login)
+  http.HandleFunc("/logout", logout)
+  http.HandleFunc("/protected", protected)
+  http.HandleFunc(":8080", nil)
+}
+
+func register(w http.ResponseWriter, r http.Request){}
+
+func login(w http.ResponseWriter, r http.Request){}
+
+func logout(w http.ResponseWriter, r http.Request){}
+
+func protected(w http.ResponseWriter, r http.Request){}
