9441940371e39a52196333d62231f44b0a99e896
Cheesydibs <9039@leerling.keizerkarelcollege.nl>
2026-05-04 13:59:49 +0200

    ioajifopuf

--- utils.go
+++ utils.go
@@ -4,4 +4,6 @@ import (
 	"golang.org/x/crypto/bcrypt" //is voor de hashing function
 )
 
-func hashPassword(password string) (st)
+func hashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+}
