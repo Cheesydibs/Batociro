52021f5ca9eec6aaa2df1b32c73fdc6386fe46bf
Tim <9084@leerling.keizerkarelcollege.nl>
2026-05-11 13:25:28 +0200

    dark/light mode

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 function changebackgroundred() {
-            document.body.style.backgroundColor = "#8f0606";
-            document.body.style.color = "#5bd5f4";
+            document.body.style.backgroundColor = "#2b2b2b";
+            document.body.style.color = "#dadada";
         }    
 
 function changebackgroundblue() {
-            document.body.style.backgroundColor = "#5bd5f4";
-            document.body.style.color = "#8f0606";
+            document.body.style.backgroundColor = "#dadada";
+            document.body.style.color = "#2b2b2b";
         }        
 
 
